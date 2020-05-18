6c232a7c42d36f4e19309c0f960d44074c828aa2
elamine370 <elamine.belgherri@gmail.com>
2020-05-19 01:57:27 +0200

    adding down up icones, and onClick events on <i> elements, events handlers are not setted yet

--- pomodoro.js
+++ pomodoro.js
@@ -9,22 +9,25 @@ class Pomodoro extends React.Component{
       }
     }
   }
+
   handleTime(){
     this.setState((state)=>{sessionLength:{sec: state.sessionLength.sec - 1}});
     if(this.state.sessionLength.sec == -1){
-
-      this.state.sessionLength.sec = 59;
-      this.setState((state)=>{sessionLength:{min: state.sessionLength.min - 1}})
+      this.setState({sessionLength: {sec: 59}});
     }
+    this.setState((state)=>{sessionLength:{min: state.sessionLength.min - 1}})
   }
+
   render(){
     return(
       <div>
         <h1>Test</h1>
         <div id="break-label">Break Length</div>
-        <div><i id="break-decrement"></i><strong id="break-length">{this.state.breakLength}</strong><i id="break-increment"></i></div>
-        <div id="session-label"><i id="session-decrement"></i>Session Length<i id="session-increment"></i></div>
-        <div><i id="session-decrement"></i><strong id="session-length">{this.state.sessionLength.min}:{this.state.sessionLength.sec}</strong><i id="session-increment"></i></div>
+        <div><i id="break-decrement" class="fas fa-angle-down" onClick="this.decrement()"></i> <strong id="break-length">{this.state.breakLength}</strong> <i id="break-increment" class="fas fa-angle-up" onClick="this.increment()"></i></div>
+        <div id="session-label"><i id="session-decrement" onClick="this.decrement()"></i>Session Length<i id="session-increment"></i></div>
+        <div><i id="session-decrement" class="fas fa-angle-down"></i> <strong id="session-length">{this.state.sessionLength.min}</strong> <i id="session-increment" class="fas fa-angle-up" onClick="this.increment()"></i></div>
+        <div >Session</div>
+        <div><strong id="session">{this.state.sessionLength.min}:{this.state.sessionLength.sec}</strong></div>
         <div>
           <div>
             <div id="timer-label"></div>
