dbec21dfa2f7e7b22d8c7611383dc2b6bc3330aa
elamine370 <elamine.belgherri@gmail.com>
2020-05-22 00:52:29 +0200

    Coding decrement and increment event handler for event click on <i> element, adding interativity for break length and session Length

--- pomodoro.js
+++ pomodoro.js
@@ -8,6 +8,11 @@ class Pomodoro extends React.Component{
       sec: 0,
       }
     }
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+  }
+  componentDidMount(){
+    document.addEventListener
   }
 
   handleTime(){
@@ -17,16 +22,35 @@ class Pomodoro extends React.Component{
     }
     this.setState((state)=>{sessionLength:{min: state.sessionLength.min - 1}})
   }
-
+  decrement(id){
+    switch(id){
+      case "break-decrement":
+        this.setState((state)=>({breakLength: state.breakLength - 1}));
+        break;
+      case "session-decrement":
+        this.setState((state)=>({sessionLength: {min: state.sessionLength.min - 1, sec: state.sessionLength.sec}}));
+        break;
+    }
+  }
+  increment(id){
+    switch(id){
+      case "break-increment":
+        this.setState((state)=>({breakLength: state.breakLength + 1}));
+        break;
+      case "session-increment":
+        this.setState((state)=>({sessionLength: {min: state.sessionLength.min + 1, sec: state.sessionLength.sec}}));
+        break;
+    }
+  }
   render(){
     return(
-      <div>
+      <div class="ml-5">
         <h1>Test</h1>
         <div id="break-label">Break Length</div>
-        <div><i id="break-decrement" class="fas fa-angle-down" onClick="this.decrement()"></i> <strong id="break-length">{this.state.breakLength}</strong> <i id="break-increment" class="fas fa-angle-up" onClick="this.increment()"></i></div>
-        <div id="session-label"><i id="session-decrement" onClick="this.decrement()"></i>Session Length<i id="session-increment"></i></div>
-        <div><i id="session-decrement" class="fas fa-angle-down"></i> <strong id="session-length">{this.state.sessionLength.min}</strong> <i id="session-increment" class="fas fa-angle-up" onClick="this.increment()"></i></div>
-        <div >Session</div>
+        <div><i id="break-decrement" class="fas fa-angle-down" onClick={()=>{this.decrement("break-decrement")}}></i> <strong id="break-length">{this.state.breakLength}</strong> <i id="break-increment" class="fas fa-angle-up" onClick={()=>{this.increment("break-increment")}}></i></div>
+        <div id="session-label"><i id="session-decrement" ></i>Session Length<i id="session-increment"></i></div>
+        <div><i id="session-decrement" class="fas fa-angle-down" onClick={()=>{this.decrement("session-decrement")}}></i> <strong id="session-length">{this.state.sessionLength.min}</strong> <i id="session-increment" class="fas fa-angle-up" onClick={()=>{this.increment("session-increment")}}></i></div>
+        <div>Session</div>
         <div><strong id="session">{this.state.sessionLength.min}:{this.state.sessionLength.sec}</strong></div>
         <div>
           <div>
